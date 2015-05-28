f0ef89d227d20a667281ee1b0abe7e85979f89df
zhazha14 <work14s@163.com>
2015-05-28 22:33:26 +0800

    ops

--- make.sh
+++ make.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 [ $# -lt 1 ] && exit 1
 file=$1
+rm -f yunwei.txt
 while read id
 do
-	curl $id
+	date=`date +%s`
+	curl -s $id -o $date.html
+	name=`sed -n "/<h1 title/,/<\/h1>/p" $date.html |grep "<div" |awk -F '[>|<]' '{print $3}'`
+	address=`grep "var address" $date.html |awk -F "\"" '{print $2}'`
+	echo "$name        $address" >>yunwei.txt
+	rm -f $date.html
 done <$file
