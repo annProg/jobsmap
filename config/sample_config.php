90b4fd9263adc1a832b6b9e2eec808b7d333f1fe
zhazha14 <work14s@163.com>
2015-05-29 11:11:40 +0800

    fix bugs

---
+++ config/sample_config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  配置文件
+ * 
+ * $Id: config.php tecbbs@qq.com 2015-5-26 $
+ **/
+
+$config = array();
+
+$config['client']['ak'] = "";
+
+$config['geocoding']['ak'] = "";
+$config['geodata']['ak'] = "";
